d516bdb08d20c8e254a47e2cc3776924c210ebdb
amks1 <amal@fieldmaster.ai>
2025-04-04 16:49:15 +0000

    Build at Fri Apr  4 16:49:15 UTC 2025

--- pywebio/__version__.py
+++ pywebio/__version__.py
@@ -1,8 +1,10 @@
 __package__ = 'pywebio'
 __description__ = 'Write interactive web app in script way.'
 __url__ = 'https://pywebio.readthedocs.io'
-__version__ = "1.8.4"
+__version__ = "1.8.4.dev2504041649"
 __version_info__ = (1, 8, 4, 0)
 __author__ = 'WangWeimin'
 __author_email__ = 'wang0.618@qq.com'
 __license__ = 'MIT'
+
+__commit_hash__ = '8b169de5'
